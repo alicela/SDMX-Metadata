13cc3242fb83f497dfa94a10882040c268e883d6
Franck Cotton <franck.cotton@insee.fr>
2020-05-23 19:42:30 +0200

    Improvement of SIMS reporting

--- src/test/java/fr/insee/semweb/sdmx/metadata/test/SIMSFrSchemeTest.java
+++ src/test/java/fr/insee/semweb/sdmx/metadata/test/SIMSFrSchemeTest.java
@@ -4,9 +4,13 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,15 +18,28 @@ import fr.insee.semweb.sdmx.metadata.Configuration;
 import fr.insee.semweb.sdmx.metadata.SIMSFrEntry;
 import fr.insee.semweb.sdmx.metadata.SIMSFrScheme;
 
+/**
+ * Test and launch methods for class <code>SIMSFrScheme</code>.
+ * 
+ * @author Franck
+ */
 public class SIMSFrSchemeTest {
 
 	SIMSFrScheme simsFrScheme = null;
+	StringBuilder report = null;
 
+	/**
+	 * Reads the SIMSFr from the Excel file.
+	 */
 	@Before
 	public void readScheme() {
 		simsFrScheme = SIMSFrScheme.readSIMSFrFromExcel(new File(Configuration.SIMS_XLSX_FILE_NAME));
+		report = new StringBuilder();
 	}
 
+	/**
+	 * Writes the SIMSFr to the console.
+	 */
 	@Test
 	public void testReadSIMSFrFromExcel() {
 
@@ -30,16 +47,24 @@ public class SIMSFrSchemeTest {
 		System.out.println(simsFrScheme);
 	}
 
+	/**
+	 * Writes the SIMSFr attribute hierarchies to the console.
+	 */
 	@Test
 	public void testGetParent() {
 
+		report.append("Attribute hierarchies found in the SIMSFr\n\nChild entry | Parent entry");
 		for (SIMSFrEntry testEntry : simsFrScheme.getEntries()) {
 			SIMSFrEntry parent = simsFrScheme.getParent(testEntry);
-			if (parent == null) System.out.println("Entry " + testEntry.getNotation() + " has no parent");
-			else System.out.println("Parent of entry " + testEntry.getNotation() + " is entry " + parent.getNotation());
+			if (parent == null) report.append('\n').append(StringUtils.rightPad(testEntry.getNotation(), 12) + "| -");
+			else report.append('\n').append(StringUtils.rightPad(testEntry.getNotation(), 12) + "| " + parent.getNotation());
 		}
+		System.out.println(report.toString());
 	}
 
+	/**
+	 * Checks that attribute hierarchies in the SIMSFr are consistent.
+	 */
 	@Test
 	public void testCheckHierarchy() {
 
@@ -48,6 +73,9 @@ public class SIMSFrSchemeTest {
 		assertEquals(hierarchyReport.length(), 0);
 	}
 
+	/**
+	 * Writes to the console the list of representations used in the base SIMS and in the SIMSFr models.
+	 */
 	@Test
 	public void testRepresentations() {
 
@@ -67,4 +95,16 @@ public class SIMSFrSchemeTest {
 		}
 		System.out.println("Insee representations " + representations);		
 	}
+
+	/**
+	 * Writes to a file the SIMSFr and the attribute hierarchies.
+	 * @throws IOException 
+	 */
+	@Test
+	public void writeReport() throws IOException {
+
+		report.append(simsFrScheme).append("\n\n");
+		testGetParent();
+		Files.write(Paths.get("src/test/resources/sims-report.txt"), report.toString().getBytes());
+	}	
 }
